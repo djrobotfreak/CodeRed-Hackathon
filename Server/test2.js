6565c8b6d2f85000b5a9ad0a4dd03642655251ae
Derek Wene <djrobotfreak@gmail.com>
2015-04-04 03:43:26 -0500

    stuff

---
+++ Server/test2.js
@@ -0,0 +1,16 @@
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+var io = require('socket.io');
+
+var app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
+
+server.listen(1357);
+
+io.sockets.on('connection', function (socket) {
+  // socket.emit('news', { hello: 'world' });
+  socket.on('GameSetup', function (data) {
+    console.log(data);
+  });
+});
